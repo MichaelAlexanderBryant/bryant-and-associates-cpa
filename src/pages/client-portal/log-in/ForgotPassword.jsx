c1a657e9300dc7815a8280c4eb7c72af676432dd
MichaelAlexanderBryant <m.alexander.bryant@gmail.com>
2023-12-16 09:04:34 -0800

    Add password reset request

--- src/pages/client-portal/log-in/ForgotPassword.jsx
+++ src/pages/client-portal/log-in/ForgotPassword.jsx
@@ -2,8 +2,19 @@ import NavigationBar from "../../../components/NavigationBar";
 import logIn from "../../../assets/log-in.jpg";
 import logIn800 from "../../../assets/log-in-800w.jpg";
 import Footer from "../../../components/Footer";
+import { useNavigate } from "react-router-dom";
+import changePassword from "../../../utils/api/ChangePassword";
 
 function ForgotPassword() {
+
+    const navigate = useNavigate();
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        let response = await changePassword(e);
+        if (response.status === 200) {
+            navigate('/log-in');
+        }
+    }
     return (
         <>
             <NavigationBar page="log-in"/>
@@ -15,7 +26,7 @@ function ForgotPassword() {
                 <h1>Client Portal</h1>
                 <div className="log-in-container">
                     <div className="form-container">
-                        <form className="log-in-form">
+                        <form className="log-in-form" onSubmit={onSubmit}>
                             <label className="log-in-username">
                                 Email:
                                 <input type="email"/>
